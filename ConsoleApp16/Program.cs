04842017400a1fc959ca099e59704d5494156696
sinagolchin <47105729+sinagolchin@users.noreply.github.com>
2019-05-07 14:56:43 -0400

    Add project files.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompt user for minutes exercise
+            System.Console.Write("Enter how many minutes you exercised: ");
+
+            string minutes = System.Console.ReadLine();
+
+            // add minute exercise to total
+            // display total minuts exercised to the screen
+            // repeat until the user quits
+        }
+    }
+}
